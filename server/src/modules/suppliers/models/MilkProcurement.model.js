d839fb4268c3eb94aad2a274637613363ff89c07
Shaheer Ali <shaheer838838@gmail.com>
2026-09-11 15:32:52 +0500

    chore: remove unused configuration file

--- server/src/modules/suppliers/models/MilkProcurement.model.js
+++
@@ -1,2 +0,0 @@
-// Re-export from central models directory to keep module self-contained
-export { MilkProcurement as default, MilkProcurement } from '../../../models/MilkProcurement.model.js';
--- server/src/modules/suppliers/models/Supplier.model.js
+++
@@ -1,2 +0,0 @@
-// Re-export from central models directory to keep module self-contained
-export { Supplier as default, Supplier } from '../../../models/Supplier.model.js';
