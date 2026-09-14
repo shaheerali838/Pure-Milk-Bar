96c91c3ee3d4589434f978494b8411ae006954bb
Nabeel87778uygf <nabeelahmad.orkz@gmail.com>
2026-09-14 13:54:26 +0500

    add the dailyClosing Module ,all work related to dailyClosing

--- server/src/app.js
+++ server/src/app.js
@@ -12,6 +12,7 @@ import adminRoutes from './modules/admin/routes/admin.routes.js';
 import customerRoutes from './modules/customers/routes/customer.routes.js';
 import deliveryRoutes from './modules/deliveries/routes/delivery.routes.js';
 import financeRoutes from './modules/finance/routes/finance.routes.js';
+import dailyClosingRoutes from './modules/dailyClosing/routes/dailyClosing.routes.js';
 
 const app = express();
 
@@ -64,6 +65,7 @@ app.use('/api/v1/admin', adminRoutes);
 app.use('/api/v1/customers', customerRoutes);
 app.use('/api/v1/deliveries', deliveryRoutes);
 app.use('/api/v1/finance', financeRoutes);
+app.use('/api/v1/daily-closings', dailyClosingRoutes);
 
 // Root route
 app.get('/', (req, res) => {
