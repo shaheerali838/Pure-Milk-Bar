3657c26c8dbc654d00ac213b9472abb1f4ee7594
Shaheer Ali <shaheer838838@gmail.com>
2026-09-22 16:49:19 +0500

    feat: add MilkingYieldLog Mongoose model

--- server/src/models/MilkingYieldLog.model.js
+++ server/src/models/MilkingYieldLog.model.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
@@ -6,31 +6,31 @@ const milkingYieldLogSchema = new Schema(
   {
     animalId: {
       type: Schema.Types.ObjectId,
-      ref: 'Animal',
-      required: [true, 'Animal ID is required'],
+      ref: "Animal",
+      required: [true, "Animal ID is required"],
       index: true,
     },
     date: {
       type: Date,
-      required: [true, 'Milking date is required'],
+      required: [true, "Milking date is required"],
       default: Date.now,
       index: true,
     },
     shift: {
       type: String,
-      required: [true, 'Milking shift is required'],
-      enum: ['MORNING', 'EVENING'],
+      required: [true, "Milking shift is required"],
+      enum: ["MORNING", "EVENING"],
       index: true,
     },
     yieldLiters: {
       type: Number,
-      required: [true, 'Yield in liters is required'],
-      min: [0, 'Yield cannot be negative'],
+      required: [true, "Yield in liters is required"],
+      min: [0, "Yield cannot be negative"],
     },
     operatorId: {
       type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: [true, 'Operator User ID is required'],
+      ref: "User",
+      required: [true, "Operator User ID is required"],
       index: true,
     },
     notes: {
@@ -46,10 +46,13 @@ const milkingYieldLogSchema = new Schema(
         return ret;
       },
     },
-  }
+  },
 );
 
-milkingYieldLogSchema.index({ animalId: 1, date: 1, shift: 1 }, { unique: true });
+milkingYieldLogSchema.index(
+  { animalId: 1, date: 1, shift: 1 },
+  { unique: true },
+);
 
-export const MilkingYieldLog = model('MilkingYieldLog', milkingYieldLogSchema);
+export const MilkingYieldLog = model("MilkingYieldLog", milkingYieldLogSchema);
 export default MilkingYieldLog;
